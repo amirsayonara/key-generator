8276aab3af5a2fe13156cb29429b3cd2af76aed2
Amir Sayonara <amirsayonara@gmail.com>
2017-12-03 16:02:16 +0700

    Penambahan fungsi format key

--- key-generator.py
+++ key-generator.py
@@ -2,17 +2,39 @@ import random
 
 class Key:
     def __init__(s, format=None, validasi=False):
-        s.karakter = [chr(x) for x in range(65,65+26)]+[str(x) for x in range(10)]
+        s.angka = [str(x) for x in range(10)]
+        s.huruf = [chr(x) for x in range(65,65+26)]
         s.format = format;s.validasi = validasi
         if not format: s.no_format()
+        elif format: s.with_format()
 
     def no_format(s):
         while True:
             s.key=''
-            for x in range(10): s.key += random.choice(s.karakter)
+            for x in range(10): s.key += random.choice(s.karakter+s.huruf)
             if not s.validasi: return
             elif s.validasi_key(): return
 
+    def with_format(s):
+        if s.validasi and not s.valid_format():
+            s.key='Format salah, pastikan setidaknya mengandung 6 angka jika format key pertama adalah angka, begitu juga dengan huruf'
+            return
+        while True:
+            s.key=''
+            for x in s.format.upper():
+                if x in s.angka: s.key += random.choice(s.angka)
+                elif x in s.huruf: s.key += random.choice(s.huruf)
+                else: s.key += x
+            if not s.validasi: return
+            elif s.validasi_key(): return
+        
+    def valid_format(s):
+        c = 0;tmp=s.format.upper()
+        for x in tmp:
+            if x in s.huruf and tmp[0] in s.huruf: c+=1
+            elif x in s.angka and tmp[0] in s.angka: c+=1
+        return c > 5
+    
     def validasi_key(s):
         return s.key.count(s.key[0])==5
 
